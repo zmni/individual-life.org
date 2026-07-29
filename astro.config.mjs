0fd69f347c13daf94121208cb6c8c9a0b03c09db
zmni <zmni@users.noreply.github.com>
2026-07-29 19:54:34 +0700

    memperbarui

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,19 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 
+import cloudflare from '@astrojs/cloudflare';
+import tailwindcss from '@tailwindcss/vite';
+
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  site: "https://individual-life.org",
+
+  adapter: cloudflare(),
+  // adapter: cloudflare({
+  //   imageService: { build: "compile", runtime: "cloudflare-binding" },
+  // }),
+
+  vite: {
+    plugins: [tailwindcss()]
+  }
+});
